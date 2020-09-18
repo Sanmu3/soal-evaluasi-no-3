b3bf4fc568ef29066b11f565f5846b7e6b089c0a
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-18 21:37:14 +0700

    soal no 2

---
+++ i.js
@@ -0,0 +1,13 @@
+const asalSantri = {
+    "Sidik": "Lampung",
+    "Wandi": "Prabumulih",
+    "Rahmad": "Medan",
+    "Usamah": "Ceribon",
+    "Syofyan": "Purwokerto",
+    "Aldi": "Kendari",
+    "Kardi": "Lombok"
+}
+
+const {Sidik, Wandi, Rahmad,Usamah, ...lain} = asalSantri;
+console.log(Sidik,Wandi,Rahmad,Usamah);
+console.log(lain);
